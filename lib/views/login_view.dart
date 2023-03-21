76046b7589e0e3b8663269c024b4f726a95c3c44
willbethetopp <muhamadtaufiksatya@gmail.com>
2023-03-21 12:16:37 +0700

    utilitiy show error dialog

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -1,8 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'dart:developer' as devtools show log;
 
 import '../constants/routes.dart';
+import '../utilities/show_error_dialog.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -65,12 +65,14 @@ class _LoginViewState extends State<LoginView> {
                     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
-                  devtools.log('User Not Found');
+                  await showErrorDialog(context, 'User not found');
                 } else if (e.code == 'wrong-password') {
-                  devtools.log('Wrong password');
+                  await showErrorDialog(context, 'Wrong credentials');
+                } else {
+                  await showErrorDialog(context, 'Error: ${e.code}');
                 }
               } catch (e) {
-                devtools.log(e.toString());
+                e.toString();
               }
             },
             child: const Text('Login'),
